dfc9e28203cc2dc2b2082cd3ba0bfc8b4e5ca7de
AhmedNour <inanuur252@gmail.com>
2022-11-11 13:51:18 +0300

    all files added

---
+++ Arrays.java
@@ -0,0 +1,7 @@
+public class Arrays {
+    public static void main(String[] args) {
+      String[] names= {"Axmed", "cabdi", "cali", "geedi"};
+      System.out.println(names[0]);
+    }
+  }
+  
---
+++ IfElse.java
@@ -0,0 +1,14 @@
+public class IfElse { 
+    public static void main(String[] args) { 
+    int mushahar = 2400; 
+    if (mushahar> 1000) { 
+    mushahar = mushahar + 300; 
+    } 
+    else { 
+    mushahar = mushahar + 200; 
+    } 
+    
+    System.out.println(mushahar); 
+    
+    } 
+   } 
